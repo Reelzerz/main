1cd278795eb5db886d6e8e6566e4b5e5f164ea67
Reelzerz <reelzerzx@gmail.com>
2017-12-20 08:04:22 -0800

    Added

--- java/reelz/mod/init/ItemInit2.java
+++ java/reelz/mod/init/ItemInit2.java
@@ -29,6 +29,7 @@ public class ItemInit2 {
 	//Material Tool
 	static final ToolMaterial  IRON_TOOL_TIER = EnumHelper.addToolMaterial("iron_tool_tier", 0, 32, 12.0F, 0.0F, 22);
 	static final ToolMaterial  STEEL_TOOL_TIER = EnumHelper.addToolMaterial("steel_tool_tier", 0, 32, 12.0F, 0.0F, 22);
+	static final ToolMaterial  ADAMANTITE_TOOL_TIER = EnumHelper.addToolMaterial("adamantite_tool_tier", 0, 32, 12.0F, 0.0F, 22);
 	//Material Armor
 	public static final ArmorMaterial IRON_ARMOR_TIER = EnumHelper.addArmorMaterial("iron_armor_tier", Reference.MODID + ":iron_armor_tier", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F);
 	public static final ArmorMaterial STEEL_ARMOR_TIER = EnumHelper.addArmorMaterial("steel_armor_tier", Reference.MODID + ":steel_armor_tier", 7, new int[]{1, 3, 5, 2}, 25, SoundEvents.ITEM_ARMOR_EQUIP_IRON, 0.0F);
@@ -52,6 +53,20 @@ public class ItemInit2 {
 	public static final Item STEEL_PICKAXE_TIER = new ToolPickaxe("steel_pickaxe_tier", STEEL_TOOL_TIER);
 	public static final Item STEEL_HOE_TIER = new ToolHoe("steel_hoe_tier", STEEL_TOOL_TIER);
 	public static final Item STEEL_SHOVEL_TIER = new ToolShovel("steel_shovel_tier", STEEL_TOOL_TIER);
+	public static final Item STEEL_HAMMER_TIER = new ToolSword("steel_hammer_tier", STEEL_TOOL_TIER);
+	public static final Item STEEL_KNIFE_TIER = new ToolSword("steel_knife_tier", STEEL_TOOL_TIER);
+	public static final Item STEEL_MACE_TIER = new ToolSword("steel_mace_tier", STEEL_TOOL_TIER);
+	public static final Item STEEL_SPEAR_TIER = new ToolSword("steel_spear_tier", STEEL_TOOL_TIER);
+	public static final Item STEEL_SWORD_TIER = new ToolSword("steel_sword_tier", STEEL_TOOL_TIER);
+	public static final Item ADAMANTITE_AXE_TIER = new ToolAxe("adamantite_axe_tier", ADAMANTITE_TOOL_TIER);
+	public static final Item ADAMANTITE_HOE_TIER = new ToolHoe("adamantite_hoe_tier", ADAMANTITE_TOOL_TIER);
+	public static final Item ADAMANTITE_PICKAXE_TIER = new ToolPickaxe("adamantite_pickaxe_tier", ADAMANTITE_TOOL_TIER);
+	public static final Item ADAMANTITE_SHOVEL_TIER = new ToolShovel("adamantite_shovel_tier", ADAMANTITE_TOOL_TIER);
+	public static final Item ADAMANTITE_SPEAR_TIER = new ToolSword("adamantite_spear_tier", ADAMANTITE_TOOL_TIER);
+	public static final Item ADAMANTITE_HAMMER_TIER = new ToolSword("adamantite_hammer_tier", ADAMANTITE_TOOL_TIER);
+	public static final Item ADAMANTITE_KNIFE_TIER = new ToolSword("adamantite_knife_tier", ADAMANTITE_TOOL_TIER);
+	public static final Item ADAMANTITE_MACE_TIER = new ToolSword("adamantite_mace_tier", ADAMANTITE_TOOL_TIER);
+	public static final Item ADAMANTITE_SWORD_TIER = new ToolSword("adamantite_sword_tier", ADAMANTITE_TOOL_TIER);
 	
 	
 	//Armor
--- java/reelz/mod/util/Reference.java
+++ java/reelz/mod/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference
 {
 	public static final String MODID = "rmumx";
 	public static final String NAME = "ReelzWorld";
-	public static final String VERSION = "0.1.1";
+	public static final String VERSION = "0.2.0";
 	public static final String CLIENT = "reelz.mod.proxy.ClientProxy";
 	public static final String COMMON = "reelz.mod.proxy.CommonProxy";
 }
